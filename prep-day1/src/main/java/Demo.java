abee028f3b6b61aa28121d6e615bd67a0e70b17b
Adelina <adelina.abdykanyeva@gmail.com>
2023-08-07 17:49:02 -0500

    with lambda

--- prep-day1/src/main/java/Demo.java
+++ prep-day1/src/main/java/Demo.java
@@ -1,5 +1,9 @@
 public class Demo {
     public static void main(String arg[]) {
+
+
+
+
         Runnable obj1 = new A();
         Runnable obj2 = new B();
 
@@ -11,3 +15,35 @@ public class Demo {
         t2.start();
     }
 }
+
+// 2nd way
+
+/*
+Runnable obj1 = () -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Hi");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        Runnable obj2 = () -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Hello");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+        t1.start();
+        t2.start();
+
+ */
